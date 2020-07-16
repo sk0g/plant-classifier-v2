f371c3e51dc966846b29010308e9640b9fbcd4da
sk0g <ani.mehta@mail.com>
2020-07-16 23:19:56 +1000

    Further progress on extracting image download URLs, and normalising species names

--- scrape.py
+++ scrape.py
@@ -37,10 +37,14 @@ def scrape_url(url: str):
             url_suffix = re.search('/[^>]*', line)
             genus_links.add(root + url_suffix[0])
 
+    # Dict[genus, Dict[species, url_set]]
+    image_links: Dict[str, Dict[str, set]] = {}
+
     # Traverse each genus index, and extract image page URLs
-    genus_image_pages: Dict[str, set] = {}
     for url in genus_links:
         current_genus = url.split("/")[-1]  # last element is species name
+        print(f"Recording image links for genus => {current_genus} ... ", end="")
+
         short_sleep(0.2)  # Please don't ban me
 
         current_genus_index = requests.get(url).text.splitlines()
@@ -49,9 +53,9 @@ def scrape_url(url: str):
                       and "/dig/" in l
                       and (  # search descriptors to weed out any sketches, microscopic images, or other misc. images
                               "fruit" in l or
+                              "plant" in l or
                               "close up" in l or
                               "flowers" in l or
-                              "whole plant" in l or
                               "leaves" in l or
                               "leaf" in l
                       )]
@@ -63,13 +67,49 @@ def scrape_url(url: str):
         image_urls = set()
         for line in href_lines:
             image_location = re.search('/[^"]*', line)[0]
-            image_urls.add(f"https://anbg.gov.au{image_location}")  # matched substring already includes leading /
+            # matched substring already includes leading forward slash, &size=3 returns the page for the bigger image
+            image_urls.add(f"https://anbg.gov.au{image_location}&size=3")
 
+        # Build the species: image_links dict
         if len(image_urls) > 1:  # Skip when one or fewer images are found
-            genus_image_pages[current_genus] = image_urls
-            print(image_urls)
+            species_links: Dict[str, set] = {}
 
-    # TODO: build map of per-species image links
+            for link in image_urls:
+                page = requests.get(link).text
+
+                # Regex magic ¯\_(ツ)_/¯
+                # Extract image href
+                image_href = re.search("/images/photo_cd/.*?jpg*", page)
+                # Extract value between h2 tag, which describes the class
+                class_tag = re.search("(?!(<h2>))[^>]*(?=(</h2>))", page)
+
+                if image_href is None or class_tag is None:
+                    raise Exception(f"Error finding class tag {class_tag} OR  image_href {image_href}")
+                else:
+                    class_tag_text = class_tag[0]
+                    # Skip image if:
+                    #   species is not defined,
+                    #   multiple classes are present
+                    #   plant is a cross species (formatted as specie1 x specie2)
+                    if len(class_tag_text.split(" ")) <= 1 or \
+                            "," in class_tag_text or \
+                            " x " in class_tag_text:
+                        continue
+
+                    # remove subspecies/ variety information, and metadata within brackets
+                    class_tag_text = re. \
+                        sub(r"(sub|var|\().*", "", class_tag_text). \
+                        replace("sp. ", "")
+
+                    if len(class_tag_text.split(" ")) != 2:
+                        print(class_tag_text)
+                    # image_url = root + image_href[0]
+
+                    short_sleep(.05)
+
+                # TODO: finish
+
+        print("done")
 
     # TODO: download images
 
